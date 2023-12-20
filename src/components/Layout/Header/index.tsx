40520b2a0521e4a89ed739e2ea6c230df97514aa
pokotaro.eth <kentarou_masuda@hacklab-tech.io>
2023-12-20 23:41:27 +0900

    feat: add layout

---
+++ src/components/Layout/Header/index.tsx
@@ -0,0 +1,5 @@
+const Header: React.FC = () => {
+  return <p>Header</p>
+}
+
+export default Header
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
+import type { ReactElement, ReactNode } from 'react'
+import type { NextPage } from 'next'
 import type { AppProps } from 'next/app'
+import { DefaultSeo } from 'next-seo'
+import { SEO } from '../../next-seo.config'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page)
+
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+      {getLayout(<Component {...pageProps} />)}
+    </>
+  )
 }
